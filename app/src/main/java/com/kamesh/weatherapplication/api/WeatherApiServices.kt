d9a5f21213fcbccd784b37875970ee405883c4c1
kamesh <kameshrajanitha@gmail.com>
2022-09-26 20:15:37 +0530

    Update WeatherApiServices.kt

--- app/src/main/java/com/kamesh/weatherapplication/api/WeatherApiServices.kt
+++ app/src/main/java/com/kamesh/weatherapplication/api/WeatherApiServices.kt
@@ -5,20 +5,10 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
-//http://api.openweathermap.org/data/2.5/weather?id=524901&appid=58714ed8d6b840338d172d0808694e3e
 
 object WeatherApiServices {
     private var retrofit:Retrofit?=null
     private val WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/"
-//    private val api = Retrofit.Builder()
-//        .baseUrl(WEATHER_BASE_URL)
-//        .addConverterFactory(GsonConverterFactory.create())
-//        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-//        .build()
-//        .create(WeatherAPI::class.java)
-//    fun getWeatherDataServices(cityName:String): Single<WeatherModel>{
-//        return api.getWeatherData(cityName)
-//    }
     fun getApiInterface(): WeatherAPI?{
         if (retrofit == null){
             retrofit = Retrofit.Builder()
